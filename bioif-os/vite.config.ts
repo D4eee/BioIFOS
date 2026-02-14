409dd695ecce26645f1a4ae6d70a7232f133a1b1
Daniel Zhou <danielshikunzhou@gmail.com>
2026-02-13 23:12:44 -0800

    update invite code

--- bioif-os/vite.config.ts
+++ bioif-os/vite.config.ts
@@ -5,6 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
+    port: 8888,
+    strictPort: true,
     proxy: {
       "/api": {
         target: "http://localhost:8000",
